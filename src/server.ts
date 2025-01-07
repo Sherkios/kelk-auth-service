f2ab7d43c9ac168bbf4aa82f73f64ae409beb9fa
Grishchenko <Sherkiosp@gmail.com>
2025-01-07 18:19:13 +0500

    убрал лишний комментарий

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import app from "src/app";
 import environment from "config/environment";
 
 const start = () => {
-  console.log("environment.port", environment.port);
-
   try {
     app.listen(environment.port, () =>
       console.log(`Server is running on http://localhost:${environment.port}`),
