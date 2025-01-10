012dd4b018e59fae673a609ef6f03882d817ffbe
Grishchenko <Sherkiosp@gmail.com>
2025-01-10 17:08:54 +0500

    Реализовал обоновление токена, внедрил redis

--- src/route/account.router.ts
+++ src/route/account.router.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import AccountController from "src/controller/account.controller";
+import authMiddleware from "src/middleware/auth-middleware";
 
 const accountRouter = Router();
 
 accountRouter.post("/account", AccountController.register);
 
 accountRouter.post("/account/login", AccountController.login);
+accountRouter.get("/account/refresh", AccountController.refreshToken);
 
-accountRouter.get("/account/:id", AccountController.getById);
+accountRouter.get("/account/:id", [authMiddleware], AccountController.getById);
 
 export default accountRouter;
--- src/service/jwt.service.ts
+++ src/service/jwt.service.ts
@@ -1,22 +1,22 @@
 import environment from "config/environment";
 import jwt from "jsonwebtoken";
+import RedisService from "service/redis.service";
+import { IJwtPayload } from "types/jwt.types";
 
-export function generateAccessToken(id: number) {
-  const payload = {
-    id,
-  };
+export default class JwtService {
+  public static generateToken(payload: IJwtPayload) {
+    const accessToken = jwt.sign(payload, environment.jwtAccessToken, { expiresIn: "1h" });
+    const refreshToken = jwt.sign(payload, environment.jwtRefreshToken, { expiresIn: "1d" });
 
-  return jwt.sign(payload, environment.jwtToken, { expiresIn: "1h" });
-}
+    RedisService.set(`account:accessToken:${accessToken}`, refreshToken, 86400);
+    return accessToken;
+  }
 
-export function generateRefreshToken(id: number) {
-  const payload = {
-    id,
-  };
+  public static getVerifyAccessToken(token: string): IJwtPayload {
+    return jwt.verify(token, environment.jwtAccessToken) as IJwtPayload;
+  }
 
-  return jwt.sign(payload, environment.jwtToken, { expiresIn: "12h" });
-}
-
-export function getVerifyToken(token: string) {
-  return jwt.verify(token, environment.jwtToken);
+  public static getVerifyRefreshToken(token: string): IJwtPayload {
+    return jwt.verify(token, environment.jwtRefreshToken) as IJwtPayload;
+  }
 }
