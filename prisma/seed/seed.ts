5af880a9d147e002ee2d09dba2f0b151dd51543c
Igor <sherkiosp@gmail.com>
2025-02-25 13:04:46 +0500

    Настроил докер, реализовал начальную миграцию и добавил сиды

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminPassword = await bcrypt.hash('admin', 10);
+  const userPassword = await bcrypt.hash('user', 10);
+
+  await prisma.user.createMany({
+    data: [
+      {
+        login: 'admin',
+        email: 'sherkios@mail.ru',
+        password: adminPassword,
+        role: 'ADMIN',
+      },
+      {
+        login: 'user',
+        email: 'user@mail.ru',
+        password: userPassword,
+        role: 'USER',
+      },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e: unknown) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
